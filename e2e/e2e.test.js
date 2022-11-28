6b3d7a2a2b7caaeeebd68c87d3306e253e7fe9f9
Vladislav <gto1103@gmail.com>
2022-11-29 00:20:57 +0300

    ok

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -16,21 +16,6 @@ describe('Test e2e for popover show', () => {
     page = await browser.newPage();
   });
 
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  //   test('should add .valid class for valid inn', async () => {
-  //     await page.goto(baseUrl);
-  //     const form = await page.$('[data-widget=innogrn-form-widget]');
-  //     const input = await form.$('[data-id=innogrn-input]');
-  //     await input.type('7715964180');
-  //     const submit = await form.$('[data-id=innogrn-submit]');
-  //     submit.click();
-  //     await page.waitFor('[data-id=innogrn-input].valid');
-  //   });
-  // });
-
   test('Click button and show popover', async () => {
     await page.goto(baseUrl);
     const button = await page.$('.button-toggle__button');
