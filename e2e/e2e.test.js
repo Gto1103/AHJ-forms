c2f7b712458a58f42dc26eb56558151bbdd78686
Vladislav <gto1103@gmail.com>
2022-11-29 00:56:19 +0300

    Ok

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -9,9 +9,9 @@ describe('Test e2e for popover show', () => {
 
   beforeAll(async () => {
     browser = await puppetteer.launch({
-      headless: false, // show gui
-      slowMo: 500,
-      devtools: true, // show devTools
+      // headless: false, // show gui
+      // slowMo: 500,
+      // devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
